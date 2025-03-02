d929d3afd51b22d2a57c53fb2a4653b510609bb8
Yerlan Alim <deus2111@gmail.com>
2025-03-02 18:19:30 +0500

    feat: update header button text and links to platform

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://bizlevel-landing-02-03-hp0admwjl-yerlanalims-projects.vercel.app',
+  base: '/',
+  output: 'static'
 });
